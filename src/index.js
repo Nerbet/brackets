b82679fcda5b3d31590efd13d0a0a56790e1f529
Nerbet <60988912+Nerbet@users.noreply.github.com>
2020-10-25 20:26:13 +0300

    Ended Task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let arrayJoin = bracketsConfig.map(function (item) {
+    return item.join('');
+  });
+
+  for (let i = 0; i < arrayJoin.length; i++) {
+    if (str.includes(arrayJoin[i]) == true) {
+      str = str.replace(arrayJoin[i], '');
+      i = -1;
+    }
+  }
+  return str == '' ? true : false;
 }
